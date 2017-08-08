3d3913ee62cce910650cb9b71274e5ccaa4ec386
LawrenceHan <yangfei6565@163.com>
2017-08-08 17:18:19 +0800

    version 1.0.0

---
+++ POPAPIKit.podspec
@@ -0,0 +1,16 @@
+Pod::Spec.new do |s|
+  s.name         = "POPAPIKit"
+  s.version      = "1.0.0"
+  s.summary      = "A Protocol Oriented Networking Kit"
+  s.description  = <<-DESC
+  POPAPIKit is a pure Swift implemented and protocol oriented networking kit. POPAPIKit is highly inspired by [APIKit](https://github.com/ishkawa/APIKit)
+  which created by [ishkawa](https://github.com/ishkawa).
+                   DESC
+  s.homepage     = "https://github.com/LawrenceHan/POPAPIKit"
+  s.license      = "MIT"
+  s.author             = { "LawrenceHan" => "yangfei6565@163.com" }
+  s.ios.deployment_target = "8.0"
+  s.source       = { :git => "https://github.com/LawrenceHan/POPAPIKit.git", :tag => "#{s.version}" }
+  s.source_files  = "POPAPIKit/Sources/**/*.{swift,h,m}"
+  s.dependency "Result", "~> 3.0"
+end
--- POPAPIKit/Sources/URLSessionClient.swift
+++ POPAPIKit/Sources/URLSessionClient.swift
@@ -1,10 +1,30 @@
+// Copyright (c) 2015 - 2016 Yosuke Ishikawa
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
 //  URLSessionClient.swift
 //  POPAPIKit
 //
-//  Created by Hanguang on 2017/8/2.
-//  Copyright © 2017年 Hanguang. All rights reserved.
+// Copyright (c) 2017年 Hanguang
 //
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 
 import Foundation
 
@@ -16,7 +36,7 @@ private var taskAssociatedObjectCompletionHandlerKey = 0
 /// `URLSessionClient` connects `URLSession` with `Session`.
 ///
 /// If you want to add custom behavior of `URLSession` by implementing delegate methods defined in
-/// `URLSessionDelegate` and related protocols, define a subclass of `URLSessionClient` and implment
+/// `URLSessionDelegate` and related protocols, define a subclass of `URLSessionClient` and implement
 /// delegate methods that you want to implement. Since `URLSessionClient` also implements delegate methods
 /// `URLSession(_:task: didCompleteWithError:)` and `URLSession(_:dataTask:didReceiveData:)`, you have to call
 /// `super` in these methods if you implement them.
